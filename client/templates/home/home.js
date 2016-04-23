adcfe8cd4d19eb0c0896309f502704a52c547388
Erick Ballesteros <erickballesteros@gmail.com>
2016-04-23 16:18:07 +0800

    client commits

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -13,4 +13,27 @@ function HomeCtrl (
     $reactive(this).attach($scope);
     var self = this;
 
-};
+    $scope.categories = [
+      {
+        "id":0,
+        "title":'Therapist',
+        "image": 'http://img08.deviantart.net/25d2/i/2009/300/7/e/naruto_sage_mode_2_by_kraytos.jpg'
+      },
+      {
+        "id":1,
+        "title":'Tutor',
+        "image": 'http://img08.deviantart.net/25d2/i/2009/300/7/e/naruto_sage_mode_2_by_kraytos.jpg'
+      },
+      {
+        "id":2,
+        "title":'Yoga',
+        "image": 'http://img08.deviantart.net/25d2/i/2009/300/7/e/naruto_sage_mode_2_by_kraytos.jpg'
+      },
+      {
+        "id":3,
+        "title":'Trainer',
+        "image": 'http://img08.deviantart.net/25d2/i/2009/300/7/e/naruto_sage_mode_2_by_kraytos.jpg'
+      }
+    ];
+
+  };
