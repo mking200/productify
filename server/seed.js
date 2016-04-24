62ce5d7083272182e24081fcfc3a7de97ab55f1d
zakir2k <zakir.burki@gmail.com>
2016-04-24 09:40:38 +0800

    behavioural/therapy text and styling

--- server/seed.js
+++ server/seed.js
@@ -44,7 +44,7 @@ if ( Categories.find().count() === 0 ) {
             categoryId: category._id,
             profileImg: 'images/instructors/ashley.png',
             name: 'Marietta Roces',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            description: 'I am a licensed Marriage & Family Therapist in the states of RI and MA, with 8 years of experience focused on assisting clients in realizing their full potential within the scoop of their own personal goals.',
             rating: '4',
             skills: ['Applied Behavior Analysis']
           },
@@ -52,7 +52,7 @@ if ( Categories.find().count() === 0 ) {
             categoryId: category._id,
             profileImg: 'images/instructors/alec.png',
             name: 'Toni Recto',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            description: 'I am a licensed Marriage and Family Therapist, in California. I have over 14 years experience working with, children,families, and individuals. I also have extensive experience working with children with learning disabilities, ADHD, angry outbursts, trauma, and loss.',
             rating: '5',
             skills: ['Applied Behavior Analysis', 'Cognitive Behaviour Therapy']
           },
@@ -60,7 +60,7 @@ if ( Categories.find().count() === 0 ) {
             categoryId: category._id,
             profileImg: 'images/instructors/arjay.png',
             name: 'Ester Orante',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            description: 'Whether you are struggling with relationship, family, or individual issues, you deserve help that is professional, intelligent, respectful, and that will make a real difference.',
             rating: '4',
             skills: ['Cognitive Behaviour Therapy','Social Learning Theory']
           },
@@ -68,7 +68,7 @@ if ( Categories.find().count() === 0 ) {
             categoryId: category._id,
             profileImg: 'images/instructors/cheene.png',
             name: 'Earlina Custodio',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            description: 'I am a professionally trained and licensed counselor and therapist. I graduated with a Doctorate in Professional Counseling and Supervision from University of West Georgia.',
             rating: '4',
             skills: ['Applied Behavior Analysis', 'Cognitive Behaviour Therapy']
           },
@@ -76,7 +76,7 @@ if ( Categories.find().count() === 0 ) {
             categoryId: category._id,
             profileImg: 'images/instructors/christian.png',
             name: 'Hugh Torres',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            description: 'Hi. My name is Ken Fields. I am a nationally certified, state licensed mental health counselor. I am available online to provide you with quality, professional online counseling. Whatever problems and troubles you are faced with, I can offer you positive, non-judgmental support and effective therapeutic intervention.',
             rating: '5',
             skills: ['Social Learning Theory']
           },
@@ -84,7 +84,7 @@ if ( Categories.find().count() === 0 ) {
             categoryId: category._id,
             profileImg: 'images/instructors/jin.png',
             name: 'Brenda Europa',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            description: 'I am a licensed clinical social worker with an extensive and diverse experience working with adults, children, couples, and families. I earned my master’s degree from the University of Chicago in Illinois in 1997. I also hold a Bachelor’s degree from Roosevelt University in Illinois with a major in psychology. I enjoy the work I do and have worked in both the public and private sectors in the fields of social services and mental health.',
             rating: '4',
             skills: ['Applied Behavior Analysis', 'Social Learning Theory']
           }
@@ -99,7 +99,7 @@ if ( Categories.find().count() === 0 ) {
             categoryId: category._id,
             profileImg: 'images/instructors/darren.png',
             name: 'Bernard Jardenil',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            description: 'Bernard started practicing Iyengar yoga in Sydney, Australia in 1986 and started his teacher training with Peter Scott in Melbourne in 1990. He completed his teacher training at the Maida Vale Iyengar Institute, London in 1994.',
             rating: '5',
             skills: ['Yin']
           },
@@ -107,7 +107,7 @@ if ( Categories.find().count() === 0 ) {
             categoryId: category._id,
             profileImg: 'images/instructors/phia.png',
             name: 'Madalena Cantillo',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            description: 'Madalena has been practicing Iyengar yoga since 1994 at the Iyengar Institute in Maida Vale. She is a qualified Junior Intermediate Level 2 teacher and continues to study under the guidance of Alaric Newcombe, one of the most senior teachers in this country.',
             rating: '4',
             skills: ['Hot']
           },
@@ -115,7 +115,7 @@ if ( Categories.find().count() === 0 ) {
             categoryId: category._id,
             profileImg: 'images/instructors/ashley.png',
             name: 'Barbara Jardenil',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            description: 'Barbara started practising yoga in her late thirties, trying several different styles before discovering Vinyasa Flow. She felt a profound connection to the practice which opened her body, heart and mind, and continues to help her overcome the challenges of modern city living.',
             rating: '4',
             skills: ['Flow']
           },
@@ -123,7 +123,7 @@ if ( Categories.find().count() === 0 ) {
             categoryId: category._id,
             profileImg: 'images/jin.jpg',
             name: 'Rosario Dulce',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            description: "Rosario's yoga practice began in 2003 with Vaccasin, at his yoga centre in Bristol. Rosario initially turned to yoga to help a rotator cuff injury and was greatly inspired by the healing transformation that took place in her body as her yoga practice deepened.",
             rating: '5',
             skills: ['Vinyasa']
           },
@@ -131,7 +131,7 @@ if ( Categories.find().count() === 0 ) {
             categoryId: category._id,
             profileImg: 'images/instructors/gab.png',
             name: 'Declan Parker',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            description: 'Declan started practicing yoga when looking for something to heal a hip injury and aid the flow of his writing. He started with Ashtanga yoga in the gym. Declan soon became passionate about yoga and its physical and mental benefits and went on to explore and study "Yoga Monks" intensively for one and a half years while also studying the philosophical side of yoga with David Sye. Declan went on to complete his yoga teacher training at Sonic Yoga in New York city in 2010.',
             rating: '4',
             skills: ['Flow']
           }
@@ -141,12 +141,12 @@ if ( Categories.find().count() === 0 ) {
         })
       }
       else {
-        for (i = 0; i < 8; i++) {
+        for (i = 1; i < 9; i++) {
           var instructor = {
             categoryId: category._id,
-            profileImg: 'images/profile.jpg',
+            profileImg: 'https://s3-ap-southeast-1.amazonaws.com/dutchbaghk/icon-profile-default-linkedin-image.png',
             name: 'Instructor ' + i,
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            description: "This profile hasn't been completed yet. Please contact the instructor for more details",
             rating: '4',
             skills: ['Skill 1', 'Skill 2', 'Skill 3']
           }
